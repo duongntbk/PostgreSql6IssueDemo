8cdc0609b0a65c81c74289f122fe1c46e6e47cea
Nguyen Thai Duong <duongnt.bk@gmail.com>
2022-10-17 00:27:24 +0900

    Fix wrong class.

--- Net6/DomainContextLegacy.cs
+++ Net6/DomainContextLegacy.cs
@@ -8,11 +8,11 @@ public class DomainContextLegacy : DbContext
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
 
-    public static DomainContextFixed Create(string conStr)
+    public static DomainContextLegacy Create(string conStr)
     {
-        var optionBuilder = new DbContextOptionsBuilder<DomainContextFixed>();
+        var optionBuilder = new DbContextOptionsBuilder<DomainContextLegacy>();
         optionBuilder.UseNpgsql(conStr, db => db.MigrationsAssembly("DbSetup"));
-        return new DomainContextFixed(optionBuilder.Options);
+        return new DomainContextLegacy(optionBuilder.Options);
     }
 
     public virtual DbSet<Person> People { get; set; }
